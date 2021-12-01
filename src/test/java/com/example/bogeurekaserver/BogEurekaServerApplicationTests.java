a6f1af9fcd12fbc246daecaf4c9bec3bfd7660fe
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-12-01 19:29:09 +0300

    Initial commit

---
+++ src/test/java/com/example/bogeurekaserver/BogEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bogeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
