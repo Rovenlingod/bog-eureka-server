1127a0a65a7be7019c5ad997ba40b2bcc2cea1c0
Daniil Kovalyov <kovalyovd21@gmail.com>
2022-04-10 14:30:44 +0300

    init

--- src/test/java/com/example/bogeurekaserver/BogEurekaServerApplicationTests.java
+++ src/test/java/com/example/bogeurekaserver/BogEurekaServerApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.bogeurekaserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BogEurekaServerApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
